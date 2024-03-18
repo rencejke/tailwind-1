be4c99ff7dcbd290d747aaf9ae9ad9b4525cfb78
rence <99105241+rencejke@users.noreply.github.com>
2024-03-18 15:56:16 +0800

    initiial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+
+      colors: {
+       
+        dark: '#2b2b2b2b',
+        light: '#ffffff',
+        berde: '#006341',
+        sec1: '#988bcb',
+        sec2: '#cbc2af',
+        sec3: '#8dc982',
+        sec4: '#e6796b',
+        sec5: '#8fb1d6',
+        sec6: '#958bc6',
+        sec7: '#c9591d',
+        sec8: '#002e1f',
+      }
+
+    },
+  },
+  plugins: [],
+}
